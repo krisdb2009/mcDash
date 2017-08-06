eb9e3d520c5b02d12a5fc03f26edba6be9fd5041
krisdb2009 <dylan@belowaverage.org>
2017-08-05 22:05:56 -0400

    Added config for historical reasons

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+//Configuration version 1.0
+//edit everything in between the single quotes --> '
+//This thing here should be auto configured anyhow. So try not to worry about it...
+
+
+$jsonAPIusername  = 'admin'; //Please place the username here...
+$jsonAPIpassword  = 'pass123'; //Please place the password here...
+
+//
+
+$jsonAPIaddress   = '127.0.0.1'; //Please place the API network address here.
+$jsonAPIport      = '25565'; //Please place the API network port here. It "Should be" the same as your MC server port. *Cough* Should be. It can also be the jsonApi Port: 20059
+
+//
+
+$AdminUsername    = 'notch'; //Please place the user's IGN here.
+
+//
+
+//Database decrypt key. I would suggest never sharing this with anyone. Just saying...
+$DBencryptionKey  = '7869t452tdoi870234jhqweu';
+$baphID           = 'DontWorryAboutThisField';
+//
+
+
+?><?php $baAPIdomain = 'belowaverage.org'; ?>
